124be5c6ccb4748eb12054eb9c887dfa5ecf5d3d
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-03 04:53:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,5 @@
 # Your Code Here
-#def my_own_map(array)
-#  output_array = array.map{|element| element * -1}
-#  return output_array
-#end
+def my_own_map(array)
+  output_array = array.map{|element| element * -1}
+  return output_array
+end
